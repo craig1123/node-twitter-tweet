b7d70286bf2415cd4f54449729249c3337b077f5
Craig Walker <craigwalker1123@gmail.com>
2018-02-03 08:01:46 -0700

    adding process.env

--- emailCtrl.js
+++ emailCtrl.js
@@ -1,18 +1,17 @@
 var nodemailer = require('nodemailer');
-var keys = require('./keys.js');
 
 var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: keys.user,
-    pass: keys.pass
+    user: process.env.USER,
+    pass: process.env.PASS
   }
 });
 
 var sendEmailToMyself = function(options) {
   transporter.sendMail({
-    from: keys.user,
-    to: keys.user,
+    from: process.env.USER,
+    to: process.env.USER,
     subject: 'Daily Code Challenge tweet was options.length characters',
     text: 'The tweet was ' + options.length + ' characters long. Go look at the number ' + options.number + '.' +
     '\n\nThen go to https://twitter.com/365daysofcode and write a new tweet'
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var twit = require('twit');
 var twitterAutofollowBot = require('twitter-autofollow-bot');
-var config = require('./config.js');
 var challenges = require('./challenges');
 var sendEmailToMyself = require('./emailCtrl');
 
-var Twitter = new twit(config);
+var Twitter = new twit({
+ consumer_key: process.env.CONSUMER_KEY,
+ consumer_secret: process.env.CONSUMER_SECRET,
+ access_token: process.env.ACCESS_TOKEN,
+ access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
 var tick = 0
 var stream = Twitter.stream('user');
 

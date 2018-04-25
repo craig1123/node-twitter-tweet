10071eb6e9a8c7339d558af07d76b2157a3a08df
Sterling Chin <schin@overstock.com>
2018-04-25 14:26:11 -0600

    move string to end of array

--- challenges.js
+++ challenges.js
@@ -28,7 +28,7 @@ module.exports = [
   "",
   "",
   "",
-  "Your goal today is to find the highest score for a blackjack hand given as strings in an array (example: ['A','K'] or ['7', '9']).  Remember aces can either score 1 or 11",
+  "",
   "Take a string of words and reverse each word. Bonus points if you don't touch punctuation.\n\nExamples:\nHello World -> olleH dlroW\nThis is a string. -> sihT si a gnirts.",
   "If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.\nFind the sum of all the multiples of 3 or 5 below 1000",
   "Suppose you have a deck of cards. You can perfectly shuffle that list by cutting it at the halfway point, then interleaving the two halves by alternating back and forth between the cards.\n\nE.g. [1 2 3 4 5 6 7 8 9 10] ---> [6 1 7 2 8 3 9 4 10 5]",
@@ -130,4 +130,5 @@ module.exports = [
   "Your goal today is to read a string of Braille characters (using standard English Braille) and print off the word in standard English letters.\n",
   "Write a program that outputs the first recurring character in a string.\n\nE.g. ABCDEBC --> B",
   "A pangram or holoalphabetic sentence for a given alphabet is a sentence using every letter of the alphabet at least once.\n\nWrite a program that takes a long sentence tells whether it is a pangram or not.",
+  "Your goal today is to find the highest score for a blackjack hand given as strings in an array (example: ['A','K'] or ['7', '9']).  Remember aces can either score 1 or 11."
 ];
